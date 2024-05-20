c2db4c6e899dffc677bfb739e6310a439564bc40
MorningStar1098 <135603807+MorningStar1098@users.noreply.github.com>
2024-05-20 15:13:53 +0530

    Delete soketfornt directory

--- soketfornt/src/components/BaseUrl.js
+++
@@ -1,7 +0,0 @@
-export const details=process.env.NODE_ENV==="development"?"http://localhost:4545/api/products/details":"https://productbackend-1.onrender.com/api/products/details";
-export const addtocartget=process.env.NODE_ENV==="development"?"http://localhost:4545/api/products/showcart":"https://productbackend-1.onrender.com/api/products/showcart"; 
-export const productpayment =process.env.NODE_ENV==="development"?"http://localhost:4545/api/products/showcart":"https://productbackend-1.onrender.com/api/products/showcart";
-export const getalluser=process.env.NODE_ENV==="development"?"http://localhost:4545/api/products/alluser":"https://productbackend-1.onrender.com/api/products/alluser";
-export const getuser=process.env.NODE_ENV==="development"?"http://localhost:4545/api/products/user":"https://productbackend-1.onrender.com/api/products/user";
-export const getmessages=process.env.NODE_ENV==="development"?"http://localhost:4545/api/products/getmessage":"https://productbackend-1.onrender.com/api/products/getmessage";
-export const LIVE_URL=process.env.NODE_ENV==="development"?"http://localhost:4545":"https://productbackend-1.onrender.comm";
